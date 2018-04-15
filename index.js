106a695bc72d60be4c18781c0f4e3b7f04bcd188
Samuel Mortenson <samuel.mortenson@acquia.com>
2018-04-15 12:10:22 -0700

    Support Webpack 4.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const loaderUtils = require('loader-utils');
 module.exports = function loader(content, map, meta) {
   const options = loaderUtils.getOptions(this);
   if (options.output) {
-    const name = loaderUtils.interpolateName(this, options.output, this.options);
+    const name = loaderUtils.interpolateName(this, options.output, options);
     this.emitFile(name, content);
   }
   this.callback(null, content, map, meta);
